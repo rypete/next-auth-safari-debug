89bbb7be260d3cd97e0c9abacff686dcdcf7e7fb
Ryan Peterson <ryan.peterson@nandos.co.uk>
2024-06-30 21:40:10 +0100

    wild debug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import "@/styles/globals.css";
 import { SessionProvider, useSession } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { PropsWithChildren, useEffect, useState } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+export const AuthContext = createContext<{
+  setValue: (n: number) => void;
+  value: number;
+}>({ value: 0, setValue: () => {} });
+
+const AuthContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [value, setValue] = useState(0);
+
+  const memo = useMemo(() => ({ value, setValue }), [value]);
+
+  return <AuthContext.Provider value={memo}>{children}</AuthContext.Provider>;
+};
 
 const DirtyHook = () => {
-  useSession();
+  const { setValue } = useContext(AuthContext);
+  const { data, status } = useSession();
 
+  useEffect(() => {
+    if (status === "authenticated") {
+      setValue(data.user.generated);
+    }
+  }, [setValue, data, status]);
   return null;
 };
 
@@ -32,8 +58,10 @@ const Provider: React.FC<PropsWithChildren> = ({ children }) => {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
+    <AuthContextProvider>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </AuthContextProvider>
   );
 }
--- src/pages/old.tsx
+++ src/pages/old.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useContext } from "react";
-import { AuthContext } from "./_app";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { value } = useContext(AuthContext);
+  const { data, status } = useSession();
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div>
-        <p>{value}</p>
-
+        <p>status: {status}</p>
+        {status == "authenticated" && (
+          <p>Pretty timestamp: {new Date(data.user.generated).toUTCString()}</p>
+        )}
+        {status == "authenticated" && <p>Timestamp: {data.user.generated}</p>}
+        {status == "authenticated" && <p>Expires: {data.user.expires}</p>}
         <div>
           <button className="m-5" onClick={() => signIn("github")}>
             Sign in
