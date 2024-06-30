8e0edf299279ae80f2f085afd0c3b3c4e49dcf0e
Ryan Peterson <ryan.peterson@nandos.co.uk>
2024-06-30 21:25:12 +0100

    dirty hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,39 @@
 import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { PropsWithChildren, useEffect, useState } from "react";
+
+const DirtyHook = () => {
+  useSession();
+
+  return null;
+};
+
+const Provider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  });
+
+  if (!loaded) {
+    return <p>not yet</p>;
+  }
+
+  return (
+    <SessionProvider refetchOnWindowFocus refetchInterval={150}>
+      <DirtyHook />
+      {children}
+    </SessionProvider>
+  );
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider refetchOnWindowFocus refetchInterval={150}>
+    <Provider>
       <Component {...pageProps} />
-    </SessionProvider>
+    </Provider>
   );
 }
